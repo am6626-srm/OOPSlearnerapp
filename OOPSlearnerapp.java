1fa9787e4b500e089344490c7fab635edafb9710
aaryanmiraje <am6626@srmist.edu.in>
2026-03-02 00:12:04 +0530

    UC2: Implement ASCII Banner for OOPS

--- OOPSlearnerapp.java
+++ OOPSlearnerapp.java
@@ -1,9 +1,25 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Display "OOPS" as ASCII Banner using '*' and spaces.
+ *
+ * @author Aaryan
+ * @version 2.0
+ */
 public class OOPSlearnerapp {
-	public static void main(String[] args) {
-		printOOPS();
-	}
 
-	private static void printOOPS() {
-		System.out.println("OOPS");
-	}
+    public static void main(String[] args) {
+        printBanner();
+    }
+
+    private static void printBanner() {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     *  *****   ***** ");
+        System.out.println("*     * *     *       *       *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   ***** ");
+    }
 }
